5076c24fcfffc74afadf77098740630a28085ab6
QureshiHK <164043943+QureshiHK@users.noreply.github.com>
2025-02-26 17:31:32 +0000

    changed bpm calc and added user input values

--- fishbpm.py
+++ fishbpm.py
@@ -7,7 +7,9 @@ from csv import writer
 
 #filepath = 'C:/Users/hkqur/Documents/python_A/fishbpm/'
 filepath = input('input full directory path here, remember to end directory in /')
-
+frametime_seconds = input('seconds = ') 
+frametime_numberofframes = input('number of frames = ')
+frametime = float(frametime_seconds)/float(frametime_numberofframes)
 
 directory_files = listdir(filepath)
 directory_files = [b for b in directory_files if b.endswith('.csv') is True]
@@ -24,10 +26,11 @@ with open(filepath+'fishbpm/fishbpm.csv', 'w', newline='') as csvfile:
             fish_df = pd.read_csv(filepath+'/'+a)
         frametimelist = fish_df['Unnamed: 2'].tolist()
         print(frametimelist)
-        fishbpm = ((fish_df.iloc[len(fish_df)-1,1]-fish_df.iloc[0,1])*fmean(frametimelist[1:]))/60
+        fishbpm_pre = ((fish_df.iloc[len(fish_df)-1,1]-fish_df.iloc[0,1])*frametime)
+        fishbpm_post = (len(fish_df)-1)*(60/fishbpm_pre)
         #print(fishbpm)
-        
     
-        writer_object.writerow([a,fishbpm])
+        writer_object.writerow([a,fishbpm_post])
+
         
        
